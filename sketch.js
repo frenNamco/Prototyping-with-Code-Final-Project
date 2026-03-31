3e83080011d347893ff5a5afec772aed6ebdf2e0
Fernando Aragon <faragonchavarria3@gmail.com>
2026-03-31 12:26:39 -0400

    renamed a couple things

--- sketch.js
+++ sketch.js
@@ -13,26 +13,34 @@ class HandTracker {
     this.video = createCapture(VIDEO, { flipped: true });
     this.video.size(w, h);
     this.video.hide();
-    // Arrow function preserves `this` context
+
     this.handPose.detectStart(this.video, (results) => {
       this.hands = results;
     });
   }
 
-  draw() {
+  drawKeypoints() {
     if (this.hands.length > 0) {
-      for (let hand of this.hands) {
-        if (hand.confidence > 0.01) {
-          for (let kp of hand.keypoints) {
-            fill(255, 0, 255);
-            noStroke();
-            circle(kp.x, kp.y, 10);
-          }
+      let hand = this.hands[0];
+
+      if (hand.confidence > 0.1) {
+        for (let kp of hand.keypoints) {
+          console.log(kp);
+          fill(255, 0, 255);
+          noStroke();
+
+           circle(kp.x, kp.y, 20);
         }
       }
     }
   }
+
+  drawCursor() {
+    
+  }
 }
+
+
 
 let tracker;
 
@@ -43,11 +51,11 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tracker.setup(width * 0.1, height * 0.1);
+  tracker.setup(width * 0.5, height * 0.5);
 }
 
 function draw() {
   background(220);
-  image(tracker.video, 0, 0, 0.1 * width, 0.1 * height);
-  tracker.draw();
+  image(tracker.video, 0, 0, 0.5 * width, 0.5 * height);
+  tracker.drawKeypoints();
 }
