2a53a0c04955d573b665e7460314a95482215849
Fernando Aragon <faragonchavarria3@gmail.com>
2026-04-03 15:16:49 -0400

    worked on tracker and cursor

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
 class HandTracker {
   constructor() {
+    this.video = null;
+    this.videoWidth = null;
+    this.videoHeight = null;
+    this.widthMod = 0.2;
+    this.heightMod = 0.2;
+
     this.hands = [];
     this.handPose = null;
-    this.video = null;
+    this.currentHand = null;
+    this.keyPointRadius = this.widthMod * 33.333;
   }
 
   preload() {
@@ -10,8 +17,10 @@ class HandTracker {
   }
 
   setup(w, h) {
+    this.videoWidth = w * this.widthMod;
+    this.videoHeight = h * this.heightMod;
     this.video = createCapture(VIDEO, { flipped: true });
-    this.video.size(w, h);
+    this.video.size(this.videoWidth, this.videoHeight);
     this.video.hide();
 
     this.handPose.detectStart(this.video, (results) => {
@@ -21,41 +30,81 @@ class HandTracker {
 
   drawKeypoints() {
     if (this.hands.length > 0) {
-      let hand = this.hands[0];
+      this.currentHand = this.hands[0];
 
-      if (hand.confidence > 0.1) {
-        for (let kp of hand.keypoints) {
-          console.log(kp);
-          fill(255, 0, 255);
+      
+      if (this.currentHand.confidence > 0.1) {
+        for (let kp of this.currentHand.keypoints) {
+          if (kp.name == "index_finger_tip") {
+            fill(0, 255, 0);
+          } else {
+            fill(255, 0, 255);
+          }
           noStroke();
 
-           circle(kp.x, kp.y, 20);
+          circle(kp.x, kp.y, this.keyPointRadius);
         }
       }
     }
   }
+}
+
+class Cursor {
+  constructor() {
+    this.x = null;
+    this.y = null;
+
+    this.radius = 20;
+    this.color = "red"
+
+    this.indexKeypointY = null;
+    this.indexKeypointX = null;
+    this.indexKeypointY = null;
+
+  }
+
+  updateCursor(tracker) {
+    if (tracker.hands[0] != undefined) {
+      this.indexKeypoint = tracker.hands[0].keypoints[8];
+      this.keypointX = this.indexKeypoint.x;
+      this.keypointY = this.indexKeypoint.y;
+
+      this.x = map(this.keypointX, 0, tracker.videoWidth, 0, width);
+      this.y = map(this.keypointY, 0, tracker.videoHeight, 0, height);
+    } else {
+      
+    }
+  }
 
   drawCursor() {
-    
+    fill(this.color);
+    noStroke();
+    circle(this.x, this.y, this.radius);
   }
 }
 
 
 
 let tracker;
+let cursor;
 
 function preload() {
   tracker = new HandTracker();
+  cursor = new Cursor();
   tracker.preload();
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tracker.setup(width * 0.5, height * 0.5);
+  tracker.setup(width, height);
 }
 
 function draw() {
   background(220);
-  image(tracker.video, 0, 0, 0.5 * width, 0.5 * height);
+  image(tracker.video, 0, 0, tracker.videoWidth, tracker.videoHeight);
   tracker.drawKeypoints();
+
+  cursor.updateCursor(tracker);
+  console.log(cursor.x);
+  cursor.drawCursor();
 }
